d434e0af52d3decc5515be77a9a31f497a3d7cab
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2016-08-30 23:12:02 +0200

    M: Fix numerical problems with integer arithmetics

--- module/modeling.c
+++ module/modeling.c
@@ -1,12 +1,12 @@
 #include "modeling.h"
 
 #define LINE_SENSOR_DISTANCE 100
+#define BLACK_CENTER_DISTANCE 200
 #define P0 (100 * 100)
-#define Bl -1
-#define Br 1
+#define B 0.1
 #define Q 10 * 10
-#define Rll 1
-#define Rrr 1
+#define Rll 5
+#define Rrr 5
 #define Rrl 0
 
 MODULE_INIT(modeling)
@@ -21,17 +21,28 @@ MODULE_INIT(modeling)
 
 static unsigned char expected_brightness(signed int x)
 {
-  if (x > -100 && x < 100) {
-    return 255;
+  if (x < -BLACK_CENTER_DISTANCE) {
+    return 0;
+  } else if (x < 0) {
+    return 255 + x;
+  } else if (x <= BLACK_CENTER_DISTANCE) {
+    return 255 - x;
+  } else {
+    return 0;
   }
-  // TODO
-  return 0;
 }
 
 static signed int ddx_expected_brightness(signed int x)
 {
-  // TODO
-  return x;
+  if (x < -BLACK_CENTER_DISTANCE) {
+    return 0;
+  } else if (x < 0) {
+    return 1;
+  } else if (x <= BLACK_CENTER_DISTANCE) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 MODULE_EXECUTE(modeling)
@@ -42,34 +53,36 @@ MODULE_EXECUTE(modeling)
     return;
   }
   // Kalman prediction
-  this->state->x += Bl * this->control->left + Br * this->control->right;
+  this->state->x += B * (this->control->right - this->control->left);
   this->state->p += Q;
   DEBUG_OUTPUT("M: Predicted state: x = %d, p = %d\n", this->state->x, this->state->p);
   // Kalman update
   // y = [ y1 y2 ]' = [ this->light->left this->light->right ]' - h(this->state->x)
-  signed int y1 = (signed int)(this->light->left) - expected_brightness(this->state->x + LINE_SENSOR_DISTANCE);
-  signed int y2 = (signed int)(this->light->right) - expected_brightness(this->state->x - LINE_SENSOR_DISTANCE);
+  const signed int y1 = (signed int)(this->light->left) - expected_brightness(this->state->x + LINE_SENSOR_DISTANCE);
+  const signed int y2 = (signed int)(this->light->right) - expected_brightness(this->state->x - LINE_SENSOR_DISTANCE);
   DEBUG_OUTPUT("M: y = [ %d %d ]\n", y1, y2);
   // H = [ h1 h2 ]' = d/dx h(this->state->x)
-  signed int h1 = ddx_expected_brightness(this->state->x + LINE_SENSOR_DISTANCE);
-  signed int h2 = ddx_expected_brightness(this->state->x - LINE_SENSOR_DISTANCE);
+  const signed int h1 = ddx_expected_brightness(this->state->x + LINE_SENSOR_DISTANCE);
+  const signed int h2 = ddx_expected_brightness(this->state->x - LINE_SENSOR_DISTANCE);
   DEBUG_OUTPUT("M: h = [ %d %d ]\n", h1, h2);
   // S = [ s11 s12; s21 s22 ] = [ h1 h2 ]' * this->state->p * [ h1 h2 ] + [ r11 r12; r12 r22 ] = [ p * h1^2 + r11, p * h1 * h2 + r12; p * h1 * h2 + r12, p * h2^2 + r22 ]
-  signed detS = this->state->p * (Rll * h2 * h2 + Rrr * h1 * h1 - 2 * h1 * h2 * Rrl) + Rll * Rrr - Rrl * Rrl;
+  const signed int detS = (signed int)(this->state->p) * (Rll * h2 * h2 + Rrr * h1 * h1 - 2 * h1 * h2 * Rrl) + Rll * Rrr - Rrl * Rrl;
   if (detS == 0) {
-    // TODO
+    DEBUG_OUTPUT("M: S is not invertible!\n");
     return;
   }
-  signed int is11 = (this->state->p * h2 * h2 + Rrr) / detS;
-  signed int is12 = -(this->state->p * h1 * h2 + Rrl) / detS;
-  signed int is22 = (this->state->p * h1 * h1 + Rll) / detS;
+  DEBUG_OUTPUT("M: det(S) = %d\n", detS);
+  const signed int is11detS = ((signed int)(this->state->p) * h2 * h2 + Rrr);
+  const signed int is12detS = -((signed int)(this->state->p) * h1 * h2 + Rrl);
+  const signed int is22detS = ((signed int)(this->state->p) * h1 * h1 + Rll);
+  DEBUG_OUTPUT("M: inv(S) * det(S) = [ %d %d; %d %d ]\n", is11detS, is12detS, is12detS, is22detS);
   // K = [ k1 k2 ] = this->state->p * [ h1 h2 ] * inv(S)
-  signed int k1 = this->state->p * (h1 * is11 + h2 * is12);
-  signed int k2 = this->state->p * (h1 * is12 + h2 * is22);
-  DEBUG_OUTPUT("M: k = [ %d %d ]\n", k1, k2);
+  const signed int k1detS = (signed int)(this->state->p) * (h1 * is11detS + h2 * is12detS);
+  const signed int k2detS = (signed int)(this->state->p) * (h1 * is12detS + h2 * is22detS);
+  DEBUG_OUTPUT("M: k * det(S) = [ %d %d ]\n", k1detS, k2detS);
   // x = x + K * y = x + [ k1 k2 ] * [ y1 y2 ]' = x + k1 * y1 + k2 * y2
-  this->state->x += k1 * y1 + k2 * y2;
+  this->state->x += (k1detS * y1 + k2detS * y2) / detS;
   // P = (I - K * H) * P = (1 - [ k1 k2 ] * [ h1 h2 ]') * P
-  this->state->p -= (k1 * h1 + k2 * h2) * this->state->p;
+  this->state->p -= (k1detS * h1 + k2detS * h2) * (signed int)(this->state->p) / detS;
   DEBUG_OUTPUT("M: Estimated state: x = %d, p = %d\n", this->state->x, this->state->p);
 }
