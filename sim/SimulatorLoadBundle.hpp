a03680ab4ea2c553e16e99982a14dcbafaf1f534
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2016-09-06 22:31:47 +0200

    SIM: Introduce SimulatorLoadBundle
    The SimulatorLoadBundle contains things that change when another
    environment is loaded.

---
+++ sim/SimulatorLoadBundle.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+
+#include <QMetaType>
+
+#include "Types.hpp"
+
+struct SimulatorLoadBundle {
+  /// the pose of the robot after resetting
+  Pose2D initial_robot_pose;
+  /// the lines on the floor
+  std::vector<Line2D> lines;
+};
+
+Q_DECLARE_METATYPE(SimulatorLoadBundle)
--- sim/main.cpp
+++ sim/main.cpp
@@ -2,11 +2,13 @@
 
 #include "MainWindow.hpp"
 #include "SimulatorCycleBundle.hpp"
+#include "SimulatorLoadBundle.hpp"
 
 int main(int argc, char* argv[])
 {
   QApplication app(argc, argv);
   qRegisterMetaType<SimulatorCycleBundle>();
+  qRegisterMetaType<SimulatorLoadBundle>();
   MainWindow window;
   window.show();
   return app.exec();
