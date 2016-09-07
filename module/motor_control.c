a6316b63cefa15071a54e5efff782f5521e583fb
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2016-09-07 15:16:29 +0200

    MC: Implement PID controller

--- module/motor_control.c
+++ module/motor_control.c
@@ -1,9 +1,15 @@
 #include "motor_control.h"
 
 // FIXME: needs to be adapted for the real robot
-#define Kl -0.5
+#define Kl -0.2
 // FIXME: needs to be adapted for the real robot
-#define Kr 0.5
+#define Kr 0.2
+// FIXME: needs to be adapted for the real robot
+#define Ti 2
+// FIXME: needs to be adapted for the real robot
+#define Td 16
+// FIXME: needs to be adapted for the real robot
+#define CObias 180
 
 MODULE_INIT(motor_control)
 {
@@ -14,28 +20,37 @@ MODULE_INIT(motor_control)
   self->control->right = 0;
   self->control->left_mode = MOTOR_BREAK;
   self->control->right_mode = MOTOR_BREAK;
+  self->integral = 0;
+  self->last_error = 0;
 }
 
-static void controller(signed int reference, signed int state, unsigned char* left, unsigned char* right)
+static void controller(motor_control_t* self, signed int reference, signed int state)
 {
   signed int error = reference - state;
   DEBUG_PRINT("MC: e = %d\n", error);
-  // P controller
-  if (Kl * error > 75) {
-    *left = 255;
-  } else if (Kl * error < -180) {
-    *left = 0;
+  self->integral += error;
+  DEBUG_PRINT("MC: integral(e) = %d\n", self->integral);
+  signed int derivative = error - self->last_error;
+  DEBUG_PRINT("MC: derivative(e) = %d\n", derivative);
+  self->last_error = error;
+  // PID controller
+  signed int left_ctrl = Kl * error + Kl * self->integral / Ti + Kl * derivative * Td;
+  if (left_ctrl > 75) {
+    self->control->left = 255;
+  } else if (left_ctrl < -CObias) {
+    self->control->left = 0;
   } else {
-    *left = 180 + Kl * error;
+    self->control->left = CObias + left_ctrl;
   }
-  if (Kr * error > 75) {
-    *right = 255;
-  } else if (Kr * error < -180) {
-    *right = 0;
+  signed int right_ctrl = Kr * error + Kr * self->integral / Ti + Kr * derivative * Td;
+  if (right_ctrl > 75) {
+    self->control->right = 255;
+  } else if (right_ctrl < -CObias) {
+    self->control->right = 0;
   } else {
-    *right = 180 + Kr * error;
+    self->control->right = CObias + right_ctrl;
   }
-  DEBUG_PRINT("MC: u = [ %d %d ]\n", *left, *right);
+  DEBUG_PRINT("MC: u = [ %d %d ]\n", self->control->left, self->control->right);
 }
 
 MODULE_EXECUTE(motor_control)
@@ -54,15 +69,15 @@ MODULE_EXECUTE(motor_control)
       self->control->right_mode = MOTOR_FORWARD;
       break;
     case REQUEST_DRIVE:
-      controller(0, self->state->x, &(self->control->left), &(self->control->right));
+      controller(self, 0, self->state->x);
       self->control->left_mode = MOTOR_FORWARD;
       self->control->right_mode = MOTOR_FORWARD;
       break;
     case REQUEST_CHECK_FINISHED:
       if (self->state->x < 0) {
-        controller(-100, self->state->x, &(self->control->left), &(self->control->right));
+        controller(self, -100, self->state->x);
       } else {
-        controller(100, self->state->x, &(self->control->left), &(self->control->right));
+        controller(self, 100, self->state->x);
       }
       self->control->left_mode = MOTOR_FORWARD;
       self->control->right_mode = MOTOR_FORWARD;
--- module/motor_control.h
+++ module/motor_control.h
@@ -9,4 +9,8 @@ MODULE(motor_control)
   REQUIRES(request)
   REQUIRES(state)
   PROVIDES(control)
+  /// the integral over the error
+  signed int integral;
+  /// the last measured error (used for derivative)
+  signed int last_error;
 END_MODULE(motor_control)
